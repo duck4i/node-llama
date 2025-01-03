994391f902d14835ca5aad9bc0bca06670722049
duck4i <mihailogazda@gmail.com>
2025-01-03 20:16:04 +0000

    working string return

--- src/npm-llama.cpp
+++ src/npm-llama.cpp
@@ -4,3 +4,13 @@ Napi::Value RunInference(const Napi::CallbackInfo &info)
 {
     return Napi::String::New(info.Env(), "Hello, World!");
 }
+
+// Module initialization
+Napi::Object Init(Napi::Env env, Napi::Object exports)
+{
+    exports.Set("RunInference", Napi::Function::New(env, RunInference));
+    return exports;
+}
+
+// Register your module
+NODE_API_MODULE(npm_llama, Init) // Make sure this line is present!
---
+++ test/index.js
@@ -0,0 +1,4 @@
+const { RunInference } = require('@duck4i/llama');
+
+console.log("Hello there", RunInference);
+console.log(`Inference: ${RunInference()}`);
